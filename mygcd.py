584d0e8eacebced8b522efa235b54f18536c26e4
mskmoorthy <mskmoorthy@gmail.com>
2015-10-06 17:16:10 -0400

    type checked

--- mygcd.py
+++ mygcd.py
@@ -1,4 +1,8 @@
 def gcd (a,b):
+    if (not ( isinstance(a,int) or isinstance(a,long))):
+        return(-1)
+    if (not ( isinstance(b, int) or isinstance(b,long))):
+        return(-1)
     if ((a==0) or (b==0)):
         return(-1)
     while(1>0):
